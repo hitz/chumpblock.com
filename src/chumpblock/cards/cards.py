562553f5d259fbc5015fcfc44f10cb68795bb9c9
Ben Hitz <hitz@genome.stanford.edu>
2013-07-16 09:14:30 -0700

    wip

--- src/chumpblock/cards/cards.py
+++ src/chumpblock/cards/cards.py
@@ -1,4 +1,6 @@
-zones = ['hand', 'library', 'graveyard', 'battlefield', 'exile']
+from enum import Enum
+zones = Enum('hand', 'library', 'graveyard', 'battlefield', 'exile')
+permanents = Enum('land', 'creature', 'enchantment', 'artifact', 'planeswalker')
 
 
 class Card(object):
@@ -26,31 +28,30 @@ class Card(object):
     def exile(self):
         self.zone = 'exile'
 
-    def play(self):
+    def play(self, context):
 
-        self.pay_cost()  # need some sort of game context object
+        self.pay_cost(context)  # need some sort of game context object
         self.zone = 'battlefield'
 
 
-class PermanentMixin(Object):
+class PermanentMixin(object):
     def __init__(self):
         self.is_permanent = True
 
 
-class ArtifactMixin(Objeect):
+class ArtifactMixin(object):
     def __init__(self):
         self.is_artifact = True
 
 
-class LegendaryMixin(Object):
+class LegendaryMixin(object):
     def __init__(self):
         self.is_legendary = True
 
-
-    def play(self:
+    def play(self, context):
         others = context.battlefield.get(self.name)
         if not others:
-            __super__.play()
+            super(Card, LegendaryMixin).play()
         else:
             self.destroy()
             [o.destroy() for o in others]
@@ -59,11 +60,16 @@ class LegendaryMixin(Object):
 class Creature(Card, PermanentMixin):
 
     def __init__(self, name, cost, spells, power, toughness, type, subtypes):
-        __super__.__init__(name, cost, spells)
+        super(Card, Creature).__init__(name, cost, spells)
         self.type = 'creature'
         self.power = power
         self.toughness = toughness
         self.damage = 0
         self.subtypes = subtypes
 
-class Enchantmente(Card, PermanentMixin):
+
+class Enchantment(Card, PermanentMixin):
+    def __init__(self, name, cost, spells, target_type):
+        super(Card, Enchantment).__init__(name, cost, spells)
+        self.type = 'enchantment'
+        self.target_type = target_type
